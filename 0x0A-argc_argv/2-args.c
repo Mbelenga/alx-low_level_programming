a15348222d2cffa9befe4e20008e6d2b2670f508
Mbelenga <mbelengaterence@gmail.com>
2023-09-06 07:46:14 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- *  * main - entry point
- *   * Description: prints all command line arguements
- *    * @argc: the count of arguements
- *     * @argv: the array of arguements
- *      * Return: 0
- *       */
+ * main - entry point
+ * Description: prints all command line arguements
+ * @argc: the count of arguements
+ * @argv: the array of arguements
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-		int i;
+	int i;
 
-			for (i = 0; i < argc; i++)
-					{
-								printf("%s\n", argv[i]);
-									}
-				return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
