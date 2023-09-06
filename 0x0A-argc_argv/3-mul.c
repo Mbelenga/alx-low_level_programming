ab93886b4cb94d65f58319c57b7feeb15fb2402d
Mbelenga <mbelengaterence@gmail.com>
2023-09-06 07:49:11 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *  * main - a program that multiplies two arguments passed into it
- *   *
- *    * @argc: holds the number of arguments passed
- *     * @argv: array pointer that holds the arguments passed
- *      *
- *       * Return: Always 0 (Success)
- *       */
+ * main - multiply 2 numbers and give result
+ * @argc: arguement count
+ * @argv: arguement array
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-		int m;
+	int num1;
+	int num2;
+	int result;
 
-			if (argc != 3)
-					{
-								printf("Error\n");
-										return (1);
-											}
-				else
-						{
-									m = (atoi(argv[1]) * atoi(argv[2]));
-											printf("%d\n", m);
-												}
-					return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
