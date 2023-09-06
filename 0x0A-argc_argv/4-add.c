c80b4885a355d2b01cfefd0bad06390eed5f1b41
Mbelenga <mbelengaterence@gmail.com>
2023-09-06 07:54:06 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- *  * main - adds positive numbers.
- *   * @argc: argument count
- *    * @argv: arguments
- *     *
- *      * Return: 0
- *       */
-int main(int argc, char **argv)
+ * main - entry point
+ * Description: adds 2 positive numbers
+ * @argc: arguement count
+ * @argv: argument array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
-		int i, n, sum = 0;
-			char *flag;
+	int sum;
+	int num;
+	int arg_index;
+	int char_index;
 
-				if (argc < 2)
-						{
-									printf("0\n");
-											return (0);
-												}
-
-					for (i = 1; argv[i]; i++)
-							{
-										n = strtol(argv[i], &flag, 10);
-												if (*flag)
-															{
-																			printf("Error\n");
-																						return (1);
-																								}
-														else
-																	{
-																					sum += n;
-																							}
-															}
-						printf("%d\n", sum);
-
-							return (0);
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (arg_index = 1; arg_index < argc; arg_index++)
+	{
+		for (char_index = 0; argv[arg_index][char_index] != '\0'; char_index++)
+		{
+			if (!isdigit(argv[arg_index][char_index]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[arg_index]);
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
