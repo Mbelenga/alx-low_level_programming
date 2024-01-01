621b2d392c02268e08357c5645cac11736404de6
Mbelenga <mbelengaterence@gmail.com>
2024-01-01 05:48:03 -0800

    a function that prints all the element of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- *  print_dlistint - prints all the elements of a dlistint_t list
- *  @h: pointer to the head of the list
+ * print_dlistint - This function prints all elements of the
+ * given doubly list
+ * @h: This is the pointer to the head
  *
- *  Return: number of nodes in the list
+ * Return: The elements of the list
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	if (h == NULL)
+		return (0);
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+
+	return (count);
 }
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,16 +1,14 @@
-
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the beginning of a dlistint_t list
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to a pointer to the head of the list
  * @n: element to be added
- * @head: pointer to a pointer to the head of a list
  *
- * Return: address of the new element
- *
+ * Return: address of the new element || NULL if it failed
  */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
@@ -18,6 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
@@ -27,4 +26,4 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	*head = new_node;
 	return (new_node);
-
+}
