5a2c304225bd80e76ee149917616fa0a26475c8d
Mbelenga <mbelengaterence@gmail.com>
2024-02-21 14:07:49 -0800

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-
-"""A function to find perimeter of an island."""
+"""Defines an island perimeter measuring function"""
 
 
 def island_perimeter(grid):
     """
-    Calculate the perimeter of the island 
+    Return the perimeter of an island described in grid
+    args: grid(list) - a list of lists of integers
     """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            # Check if the current cell is land (1)
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # Check adjacent cells (up, down, left, right)
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
