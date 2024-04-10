ec655646fc66c0c1404430874a6244065244b3d7
Mbelenga <mbelengaterence@gmail.com>
2024-04-10 12:46:50 -0700

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * jump_search - searches for a value in a sorted array of integers using the Jump search algorithm
+ * jump_search - searches for a value in a sorted array
  * @array: pointer to the fist element of an array
  * @size: number of elements of an array
  * @value: value to search for
